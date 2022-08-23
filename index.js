3848bcaf41a677e55fc1df07e09b60a85bfcf7d2
kimuit117 <kemoikiuita@gmail.com>
2022-08-23 12:10:42 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,18 @@
+const divide = (num1, num2)=>{
+    console.log("dividing ${num1}");
+    console.log("by ${num2}");
+    num1=2000;
+    num2=100;
+    return num1/num2;
+}
+
+
+// arrow function called square 
+
+const square = (a)=>{
+    return a*a
+};
+// arrowFunctionCalledAdd
+
+const add = (a, b) => {
+    return a+b};
